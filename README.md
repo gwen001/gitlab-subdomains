f23511b1da50df8a8f52cad77ec70a88013040ac
Gwen <g@10degres.net>
2022-11-26 19:34:21 +0100

    readme

---
+++ README.md
@@ -0,0 +1,75 @@
+<h1 align="center">gitlab-subdomains</h1>
+
+<h4 align="center">Find subdomains on GitLab.</h4>
+
+<p align="center">
+    <img src="https://img.shields.io/badge/go-v1.13-blue" alt="go badge">
+    <img src="https://img.shields.io/badge/license-MIT-green" alt="MIT license badge">
+    <a href="https://twitter.com/intent/tweet?text=https%3a%2f%2fgithub.com%2fgwen001%2fgitlab-subdomains%2f" target="_blank"><img src="https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Fgithub.com%2Fgwen001%2Fgitlab-subdomains" alt="twitter badge"></a>
+</p>
+
+<!-- <p align="center">
+    <img src="https://img.shields.io/github/stars/gwen001/gitlab-subdomains?style=social" alt="github stars badge">
+    <img src="https://img.shields.io/github/watchers/gwen001/gitlab-subdomains?style=social" alt="github watchers badge">
+    <img src="https://img.shields.io/github/forks/gwen001/gitlab-subdomains?style=social" alt="github forks badge">
+</p> -->
+
+---
+
+## Install
+
+```
+go install github.com/gwen001/gitlab-subdomains@latest
+```
+
+or
+
+```
+git clone https://github.com/gwen001/gitlab-subdomains
+cd gitlab-subdomains
+go install
+```
+
+## Usage
+
+```
+$ gitlab-subdomains -h
+
+Usage of gitlab-subdomains:
+  -d string
+    	domain you are looking for (required)
+  -debug
+    	debug mode
+  -e	extended mode, also look for <dummy>example.<tld>
+  -t string
+    	gitlab token (required), can be:
+    	  • a single token
+    	  • a list of tokens separated by comma
+    	  • a file containing 1 token per line
+    	if the options is not provided, the environment variable GITLAB_TOKEN is readed, it can be:
+    	  • a single token
+    	  • a list of tokens separated by comma
+```
+
+If you want to use multiple tokens, you better create a `.tokens` file in the executable directory with 1 token per line  
+```
+token1
+token2
+...
+```
+or use an environment variable with tokens separated by comma:  
+```
+export GITLAB_TOKEN=token1,token2...
+```
+
+Tokens are disabled when GitLab raises a rate limit alert, however they are re-enable 1mn later.
+You can disable that feature by using the option `-k`.
+
+---
+
+<img src="https://raw.githubusercontent.com/gwen001/gitlab-subdomains/master/preview.gif">
+
+---
+
+Feel free to [open an issue](/../../issues/) if you have any problem with the script.  
+
---
+++ main.go
@@ -0,0 +1,422 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"math/rand"
+	"time"
+	"flag"
+	"regexp"
+	"strings"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/logrusorgru/aurora"
+	tld "github.com/jpillora/go-tld"
+)
+
+type Token struct {
+	datoken string
+	disabled_ts int64
+}
+
+type Search struct {
+	signature string
+	scope string
+	keyword string
+	sort string
+	order_by string
+	TotalCount int
+}
+
+type Config struct {
+	stop_notoken bool
+	quick_mode bool
+	domain string
+	output string
+	fpOutput *os.File
+	tokens []Token
+	extend bool
+	debug bool
+	search string
+	delay time.Duration
+	DomainRegexp *regexp.Regexp
+}
+
+var au = aurora.NewAurora(true)
+var config = Config{}
+var t_subdomains []string
+var t_search []Search
+var t_scopes = []string{"projects","issues","merge_requests","milestones","snippet_titles","users","blobs","commits","notes","wiki_blobs"}
+// var t_scopes = []string{"issues"}
+// var t_search_fields = []string{"description"}
+
+
+func parseToken( token string ) {
+
+	if token == "" {
+		token = os.Getenv("GITLAB_TOKEN")
+		if token == "" {
+			token = readTokenFromFile(".tokens")
+			if token == "" {
+				flag.Usage()
+				fmt.Printf("\ntoken not found\n")
+				os.Exit(-1)
+			}
+		}
+	} else {
+		if _, err := os.Stat(token); os.IsNotExist(err) {
+			// path/to/whatever does not exist
+		} else {
+			token = readTokenFromFile( token )
+		}
+	}
+
+	var t_tokens = strings.Split(token, ",")
+	// var re = regexp.MustCompile(`[0-9a-f]{40}|glpat_[a-zA-Z0-9\-]`)
+
+	for _,t := range t_tokens {
+		if len(t) > 0 {
+			config.tokens = append( config.tokens, Token{datoken:t,disabled_ts:0} )
+		}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(config.tokens), func(i, j int) { config.tokens[i], config.tokens[j] = config.tokens[j], config.tokens[i] })
+}
+
+func readTokenFromFile( tokenfile string ) string {
+
+	b, err := ioutil.ReadFile( tokenfile )
+
+    if err != nil {
+        return ""
+    }
+
+	var t_token []string
+
+	for _,l := range strings.Split(string(b), "\n") {
+		l = strings.TrimSpace( l )
+		if len(l) > 0 && !inArray(l,t_token) {
+			t_token = append(t_token, l)
+		}
+	}
+
+	return strings.Join(t_token, ",")
+}
+
+
+func getNextToken( token_index int, n_token int ) int {
+
+	token_index = (token_index+1) % n_token
+
+	for k:=token_index ; k<n_token ; k++ {
+		if config.tokens[k].disabled_ts == 0 || config.tokens[k].disabled_ts < time.Now().Unix() {
+			config.tokens[k].disabled_ts = 0
+			return k
+		}
+	}
+
+	return -1
+}
+
+
+func doSearch(current_search Search) {
+
+	var page = 1
+	var token_index = -1
+	var n_token = len(config.tokens)
+
+	for run:=true; run; {
+
+		time.Sleep( config.delay * time.Millisecond )
+
+		token_index = getNextToken( token_index, n_token )
+
+		if token_index < 0 {
+			token_index = -1
+
+			if( config.stop_notoken ) {
+				PrintInfos("error", "no more token available, exiting")
+				os.Exit(-1)
+			}
+
+			PrintInfos("error", "no more token available, waiting for another available token...")
+			continue
+		}
+
+		var t_json = doRequest( config.tokens[token_index].datoken, current_search, page )
+		var n_results = len(t_json)
+
+		if n_results <= 0 {
+			run = false
+			break
+		}
+
+		doRegexp( t_json )
+
+		page++
+		// run = false
+	}
+}
+
+
+func doRequest(token string, current_search Search, page int) []map[string]interface {} {
+
+	defer func() {
+        if r := recover(); r != nil {
+            // fmt.Println("Recovered in f", r)
+        }
+    }()
+
+	var url = fmt.Sprintf("https://gitlab.com/api/v4/search?scope=%s&search=%s&order_by=%s&sort=%s&page=%d", current_search.scope, current_search.keyword, current_search.order_by, current_search.sort, page )
+	// var url = fmt.Sprintf("https://gitlab.com/api/v4/search?scope=blobs&search=%s&order_by=%s&sort=%s&page=%d", current_search.keyword, current_search.order_by, current_search.sort, page )
+	PrintInfos( "debug", url )
+
+	client := http.Client{ Timeout: time.Second * 5 }
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		PrintInfos( "error", fmt.Sprintf("%s",err) )
+	}
+
+	req.Header.Set("PRIVATE-TOKEN", token)
+	// req.Header.Set("PRIVATE-TOKEN", "fake")
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		PrintInfos( "error", fmt.Sprintf("%s",getErr) )
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		PrintInfos( "error", fmt.Sprintf("%s",readErr) )
+	}
+	// fmt.Printf("%s\n", body)
+
+	var t_json []map[string]interface {}
+
+	var t_json_error map[string]interface {}
+	jErr := json.Unmarshal([]byte(body), &t_json_error)
+	if jErr == nil {
+		PrintInfos( "error", fmt.Sprintf("%s",t_json_error["message"]) )
+	} else {
+		jErr2 := json.Unmarshal([]byte(body), &t_json)
+		if jErr2 != nil {
+			PrintInfos( "error", fmt.Sprintf("%s",jErr2) )
+		}
+	}
+
+	return t_json
+}
+
+
+func doRegexp(t_json []map[string]interface {}) {
+
+	var t_match [][]byte
+
+	for _,t_item := range t_json {
+		for _,value := range t_item {
+
+			t_match = performRegexp( fmt.Sprintf("%s",value), config.DomainRegexp )
+
+			if len(t_match) > 0 {
+				for _, match := range t_match {
+					var str_match = cleanSubdomain( match )
+					if !inArray(str_match,t_subdomains) {
+						t_subdomains = append( t_subdomains, str_match )
+						if inArrayKey("web_url",t_item) {
+							PrintInfos( "info", fmt.Sprintf("%s",t_item["web_url"]) )
+						}
+						PrintInfos( "found", str_match )
+						config.fpOutput.WriteString(str_match+"\n")
+						config.fpOutput.Sync()
+					}
+				}
+			}
+		}
+	}
+}
+
+
+func cleanSubdomain(sub []byte) string {
+	var clean_sub = string(sub)
+	clean_sub = strings.ToLower( clean_sub )
+	clean_sub = strings.TrimLeft( clean_sub, "." )
+	if strings.Index(clean_sub,"2f") == 0 {
+		clean_sub = clean_sub[2:]
+	}
+	if strings.Index(clean_sub,"252f") == 0 {
+		clean_sub = clean_sub[4:]
+	}
+	var re = regexp.MustCompile( `^u00[0-9a-f][0-9a-f]` )
+	clean_sub = re.ReplaceAllString( clean_sub, "" )
+
+	return clean_sub
+}
+
+
+func main() {
+
+	var token string
+
+	flag.StringVar( &config.domain, "d", "", "domain you are looking for (required)" )
+	flag.BoolVar( &config.extend, "e", false, "extended mode, also look for <dummy>example.com" )
+	flag.BoolVar( &config.debug, "debug", false, "debug mode" )
+	flag.StringVar( &token, "t", "", "gitlab token (required), can be:\n  • a single token\n  • a list of tokens separated by comma\n  • a file containing 1 token per line\nif the options is not provided, the environment variable GITLAB_TOKEN is readed, it can be:\n  • a single token\n  • a list of tokens separated by comma" )
+	flag.Parse()
+
+	if config.domain == "" {
+		flag.Usage()
+		fmt.Printf("\ndomain not found\n")
+		os.Exit(-1)
+	}
+
+	dir, _ := os.Getwd()
+	config.output = dir + "/" + config.domain + ".txt"
+
+	fp, outErr := os.Create( config.output )
+	if outErr != nil {
+		fmt.Println(outErr)
+		os.Exit(-1)
+	}
+
+	config.fpOutput = fp
+	// defer fp.Close()
+
+	u, _ := tld.Parse("http://"+config.domain)
+	// fmt.Println(u.Domain)
+	// fmt.Println(u.Subdomain)
+
+	if config.extend {
+		// extended mode activated
+		config.search = u.Domain
+		config.DomainRegexp = regexp.MustCompile( `(?i)[0-9a-z\-\.]+\.([0-9a-z\-]+)?`+u.Domain+`([0-9a-z\-\.]+)?\.[a-z]{1,5}`)
+	} else {
+		if( len(u.Subdomain) == 0 ) {
+			// no extended mode and no subdomain
+			config.search = u.Domain + "." + u.TLD
+			config.DomainRegexp = regexp.MustCompile( `(?i)(([0-9a-z\-\.]+)\.)?` + u.Domain + "\\." + u.TLD )
+			} else {
+			// no extended mode but subdomain is provided
+			config.search = u.Subdomain + "." + u.Domain + "." + u.TLD
+			config.DomainRegexp = regexp.MustCompile( `(?i)(([0-9a-z\-\.]+)\.)?` + u.Subdomain + "\\." + u.Domain + "\\." + u.TLD )
+		}
+	}
+
+	// fmt.Println(config.search)
+	// fmt.Println(config.DomainRegexp)
+	// os.Exit(-1)
+
+	config.search = "%22" + strings.ReplaceAll(url.QueryEscape(config.search), "-", "%2D") + "%22"
+
+	parseToken( token )
+
+	if config.debug {
+		banner()
+	}
+
+	var n_token = len(config.tokens)
+	if n_token == 0 {
+		flag.Usage()
+		PrintInfos( "error", "token not found" )
+		os.Exit(-1)
+	}
+
+	config.delay = time.Duration( 60.0 / (30*float64(n_token)) * 1000 + 200)
+
+	displayConfig()
+
+	// https://docs.gitlab.com/ee/api/search.html
+	// Allowed values are created_at only. If not set, results are sorted by created_at in descending order for basic search, or by the most relevant documents for Advanced Search.
+	var order_by = "created_at"
+	// Allowed values are asc or desc only. If not set, results are sorted by created_at in descending order for basic search, or by the most relevant documents for Advanced Search.
+	var sort = "desc"
+	var n_search = 0
+
+	for _,scope := range t_scopes {
+		var current_search = Search{scope:scope, keyword:config.search, order_by:order_by, sort:sort}
+		PrintInfos( "debug", fmt.Sprintf("scope:%s, keyword:%s, order_by:%s, sort:%s", current_search.scope, current_search.keyword, current_search.order_by, current_search.sort) )
+
+		doSearch( current_search )
+		n_search++
+	}
+
+	PrintInfos( "", fmt.Sprintf("%d searches performed",n_search) )
+	PrintInfos( "", fmt.Sprintf("%d subdomains found",len(t_subdomains)) )
+}
+
+
+func inArray(str string, array []string) bool {
+	for _,i := range array {
+		if i == str {
+			return true
+		}
+	}
+	return false
+}
+func inArrayKey(str string, array map[string]interface {}) bool {
+	for i,_ := range array {
+		if i == str {
+			return true
+		}
+	}
+	return false
+}
+
+
+func performRegexp(code string, rgxp *regexp.Regexp ) [][]byte {
+	return rgxp.FindAll([]byte(code), -1)
+}
+
+
+
+func resliceTokens(s []Token, index int) []Token {
+    return append(s[:index], s[index+1:]...)
+}
+
+
+func displayConfig() {
+	PrintInfos( "", fmt.Sprintf("Domain:%s, Output:%s",config.domain,config.output) )
+	PrintInfos( "", fmt.Sprintf("Tokens:%d, Delay:%.0fms",len(config.tokens),float32(config.delay)) )
+	PrintInfos( "", fmt.Sprintf("Token rehab:%t, Quick mode:%t",!config.stop_notoken,config.quick_mode) )
+}
+
+
+func PrintInfos(infos_type string, str string) {
+
+	if !config.debug && infos_type == "found" {
+		fmt.Println( str )
+	} else if config.debug {
+		str = fmt.Sprintf("[%s] %s", time.Now().Format("15:04:05"), str )
+
+		switch infos_type {
+			case "debug":
+				fmt.Println( au.Gray(13,str).Bold() )
+			case "info":
+				fmt.Println( au.Yellow(str).Bold() )
+			case "found":
+				fmt.Println( au.Green(str).Bold() )
+			case "error":
+				fmt.Println( au.Red(str).Bold() )
+			default:
+				fmt.Println( au.White(str).Bold() )
+		}
+	}
+}
+
+func banner() {
+	fmt.Print("\n")
+	fmt.Print(`
+  	   ▗▐  ▜    ▌          ▌    ▌          ▗
+	▞▀▌▄▜▀ ▐ ▝▀▖▛▀▖  ▞▀▘▌ ▌▛▀▖▞▀▌▞▀▖▛▚▀▖▝▀▖▄ ▛▀▖▞▀▘
+	▚▄▌▐▐ ▖▐ ▞▀▌▌ ▌  ▝▀▖▌ ▌▌ ▌▌ ▌▌ ▌▌▐ ▌▞▀▌▐ ▌ ▌▝▀▖
+	▗▄▘▀▘▀  ▘▝▀▘▀▀   ▀▀ ▝▀▘▀▀ ▝▀▘▝▀ ▘▝ ▘▝▀▘▀▘▘ ▘▀▀
+	`)
+	fmt.Print("       by @gwendallecoguic                          \n\n")
+}
